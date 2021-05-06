33506804055deeded57a75432c652f310ec89a6b
Maida Sijaric <maidasijaric@maidas-mbp.lan>
2021-05-05 21:20:16 -0400

    fixed grammar issues on meet the team + made it consistent

--- front-end/src/pages/MeetTheTeam.js
+++ front-end/src/pages/MeetTheTeam.js
@@ -14,29 +14,29 @@ const MeetTheTeam = () => {
           <h2>Raj Bharaj</h2>
           <img alt="Raj" src={rajPic} />  
           <p>
-            Senior at NYU CAS majoring in Computer Science, with minors in Mathematics, Cybersecurity and 'Web Development and Programming'.
+          Hello, I'm a senior at NYU CAS majoring in Computer Science, with minors in Mathematics, Cybersecurity and 'Web Development and Programming'.
           </p>
 
           <h2>Alifa Faruk</h2>
           <img alt="Alifa" src={alifaPic} />
           <p>
-          Senior at New York University CAS majoring in Computer Science, with a minor in Mathematics.
+          Hello, I'm a senior at New York University CAS majoring in Computer Science, with a minor in Mathematics.
           </p>
 
           <h2>Adam Ethan</h2>
           <img alt="Adam" src={adamPic} />
           <p>
-           Hello, Im a junior at NYU majoring in Computer Science and minoring in Business Studies.  
+           Hello, I'm a junior at NYU majoring in Computer Science and minoring in Business Studies.  
           </p>
           <h2>Maya Sijaric</h2>
           <img alt="Maya" src={mayaPic} />
           <p>
-           Hello Im Maya Sijaric and I'm a senior majoring in Computer Science and Politics, with a minor in Molecular and Cell Biology. 
+           Hello, I'm Maya Sijaric and I'm a senior majoring in Computer Science and Politics, with a minor in Molecular and Cell Biology. 
           </p>
           <h2>Roman Haberli</h2>
           <img alt="Roman" src={samplepics} />
           <p>
-           Hello Im Roman Haberli and Im a senior at NYU studying stuff. 
+           Hello, I'm Roman Haberli and I'm a senior at NYU studying stuff. 
           </p>
       </section>
   </div>
